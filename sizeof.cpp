7a90df3968d4d1f98ac7c10365cc9c88fe1d93c0
Mark Tobler <marktobler@gmail.com>
2020-10-30 10:02:33 -0700

    basic datatype sizes in separate tiny program

---
+++ sizeof.cpp
@@ -0,0 +1,20 @@
+/* Mark Tobler 
+ * SizeOf. Local datatypes sizes 
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+   // simple size of outputs
+   cout << "Double: " << sizeof(double) << endl;
+   cout << "Long: " << sizeof(long) << endl;
+   cout << "Int: " << sizeof(int) << endl;
+   cout << "Char: " << sizeof(char) << endl;
+ 
+
+   return 0;
+}
+
